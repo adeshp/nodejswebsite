fbb5c9a3b6b36a88f50fbd3f79a5d2ab94a6bac9
adeshp <akshaytanoor@gmail.com>
2018-08-19 09:07:54 -0500

    question_answer

--- Routes/question_answer.js
+++ Routes/question_answer.js
@@ -3,6 +3,12 @@ var router = express.Router();
 
 
 router.get('/question_answer', function (req, res) {
+    console.log(req);
+    if (req.query.d.includes('Please return OK')) {
+        res.send('ok');
+    } else {
+        res.send('Could not understand your query.');
+    }
     
 });
 
